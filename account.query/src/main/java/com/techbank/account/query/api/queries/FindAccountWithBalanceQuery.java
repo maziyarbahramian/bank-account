1a13e7760ff33da156e4d07ddc198b11cec6c5a7
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2025-08-20 23:58:47 +0330

    Queries added

---
+++ account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountWithBalanceQuery.java
@@ -0,0 +1,8 @@
+package com.techbank.account.query.api.queries;
+
+import com.techbank.account.query.api.dto.EqualityType;
+
+public class FindAccountWithBalanceQuery {
+    private EqualityType equalityType;
+    private double balance;
+}
