87e605042281391ab81911d7df92006c1c02083c
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2025-08-21 15:48:08 +0330

    AccountQueryHandler implemented

--- account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountByHolderQuery.java
+++ account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountByHolderQuery.java
@@ -6,6 +6,6 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class FindAccountByHolder extends BaseQuery {
+public class FindAccountByHolderQuery extends BaseQuery {
     private String accountHolder;
 }
--- account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountWithBalanceQuery.java
+++ account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountWithBalanceQuery.java
@@ -1,7 +1,11 @@
 package com.techbank.account.query.api.queries;
 
 import com.techbank.account.query.api.dto.EqualityType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class FindAccountWithBalanceQuery {
     private EqualityType equalityType;
     private double balance;
